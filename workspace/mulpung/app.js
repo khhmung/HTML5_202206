0e9c53d0bc6b5cd5b83ffaf55e5f56b43ff11316
hem_ung <opwera656@gmail.com>
2022-06-28 10:39:11 +0900

    logger의 위치 변경

--- workspace/mulpung/app.js
+++ workspace/mulpung/app.js
@@ -13,13 +13,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+// app.use(미들웨어);
+// 순서대로 처리한다.
+// app.use(logger('dev'));  // 로그를 제일 먼저 놓게 되면 정적인 파일 가져오는 것도 찍히기 때문에 불필요한 내용도 찍힌다.
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));  // 파일 형식으로 저장되어있는 정적인 파일을 찾아라!
 
-app.use('/', indexRouter);
+app.use(logger('dev')); // 동적인 파일만 로그에 찍히도록 한다.
+
+app.use('/', indexRouter); // public 에서 못찾았으면 / 에서 찾아라!
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
