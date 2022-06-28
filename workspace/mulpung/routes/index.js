ffbaf5746d492f2d2c84c7f62aae905ec6e8992a
hem_ung <opwera656@gmail.com>
2022-06-28 13:46:03 +0900

    .

--- workspace/mulpung/routes/index.js
+++ workspace/mulpung/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const model = require('../model/mulpangDao');
+const MyUtil = require('../utils/myutil');
 
 /* GET home page. */
+// /today.html 에서 확장자를 뗀 이유는 
+// 도메인에 파일의 확장자가 보이기 때문에 보안상의 문제가 될 수 있어 생략
+// 만약 public 정적파일에 같은 이름이 있다면 public의 파일 확장자(.html) 를 가져온다.
 router.get('/', function(req, res, next) {
-  res.redirect('/today.html');
+  res.redirect('/today');
+});
+
+// 오늘 메뉴
+router.get('/today', async function(req, res, next) {
+  const list = await model.couponList();
+  res.render('today', {list : list});
+});
+
+// 쿠폰 상세조회
+router.get('/coupons/:_id', async function(req, res, next) {
+  const coupon = await model.couponDetail(req.params._id);
+  res.render('detail', {coupon : coupon, toStar : MyUtil.toStar});
 });
 
 // get방식으로 main 페이지를 요청하면 해당페이지로 렌더링해라.
