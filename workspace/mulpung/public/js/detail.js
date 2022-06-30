119d30b23d71d842568d3a2f920243fd629037bc
hem_ung <opwera656@gmail.com>
2022-06-30 13:14:20 +0900

    관심쿠폰 등록 및 삭제 기능 추가

--- workspace/mulpung/public/js/detail.js
+++ workspace/mulpung/public/js/detail.js
@@ -31,6 +31,9 @@ function setCloseEvent(){
 
 // 관심쿠폰 등록 이벤트
 function setAddCartEvent(){
+  $('.btn_add_cart').on('click', function(){
+    addCart($(this).parent());
+  });
 	
 }
 
@@ -41,7 +44,7 @@ function addCart(coupon){
   var couponImg = coupon.children('.list_img').attr('src');
   
   // TODO 관심 쿠폰 목록을 localStorage에서 꺼낸다.
-  
+  var cart = JSON.parse(localStorage.getItem('cart') || '{}');
   
   if(Object.keys(cart).length == 5){
     alert('관심 쿠폰은 최대 5개 등록 가능합니다.');
@@ -49,7 +52,15 @@ function addCart(coupon){
     alert(couponName + '\n이미 등록되어 있습니다.');
   }else{
     // TODO 관심 쿠폰을 localStorage에 저장한다.
-
+    // 카트 객체에 속성(name, img, noti)을 만들어줌
+    cart[couponId] = {
+      name : couponName,
+      img : couponImg,
+      noti : 10
+    };
+    localStorage.setItem('cart' , JSON.stringify(cart));
+    alert(couponName + '\n관심쿠폰으로 등록되었습니다.');
+    common.cart.showCart();
     // TODO 알림메세지 사용 여부 체크
     
   }
